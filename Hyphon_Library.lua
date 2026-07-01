803788f09bf027cace3da498bd15b6f99c4d519e
Drapak <140055011+0drp@users.noreply.github.com>
2026-07-01 12:51:07 +0200

    Update Hyphon_Library.lua

--- Hyphon_Library.lua
+++ Hyphon_Library.lua
@@ -306,7 +306,7 @@ do
 
     if not startup_args.ignoreui then
         library.screengui = Instance.new('ScreenGui')
-        protect_gui(library.screengui)
+        --protect_gui(library.screengui)
         library.screengui.Parent = game:GetService('CoreGui')
 
         local button = Instance.new('ImageButton')
